4fc6bb2bba52f755f95509d01f9093d2021a1136
Max <durnenko.maxim@yandex.ru>
2017-04-13 20:11:06 +0300

    tims

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 4,
+    margin: 30,
+    loop: true,
+    nav:true,
+    navText: ['',''],
+    dots: true,
+    dotsEach: true,
+});
+});
